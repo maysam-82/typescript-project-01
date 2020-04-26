10978766e0642aea2bea1bbfa0f29a6a6bd29dd5
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-04-26 23:28:38 +1000

    initial implementation of sorting algorithm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('object');
+class Sorter {
+	constructor(public collection: number[]) {}
+	public sort(): void {
+		const { length } = this.collection;
+		for (let i = 0; i < length; i++) {
+			for (let j = 0; j < length - i - 1; j++) {
+				if (this.collection[j] > this.collection[j + 1]) {
+					const leftHand = this.collection[j];
+					this.collection[j] = this.collection[j + 1];
+					this.collection[j + 1] = leftHand;
+				}
+			}
+		}
+	}
+}
+
+const sorter = new Sorter([10, -5, 0, 3]);
+sorter.sort();
+console.log(sorter.collection);
