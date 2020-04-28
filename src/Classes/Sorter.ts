2350dc56521f578ddbe77036d6311c2509497858
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-04-28 23:43:18 +1000

    remove interface

--- src/Classes/Sorter.ts
+++ src/Classes/Sorter.ts
@@ -1,10 +1,3 @@
-// Defining interface here means defining some instructions on top of the Sorter class that say if you just give use the length property, compare and swap methods, you can use this class to sort your data regardless of what data structure is.
-interface ISorter {
-	length: number;
-	// the name of arguments can not be with the name of arguments in the actual implementation. But it is better to be because of better understanding of the code.
-	compare(leftIndex: number, rightIndex: number): boolean;
-	swap(leftIndex: number, rightIndex: number): void;
-}
 export abstract class Sorter {
 	// `abstract` is used to mark certain methods as existing in the future or essentially implemented by some child class.
 	abstract compare(leftIndex: number, rightIndex: number): boolean;
